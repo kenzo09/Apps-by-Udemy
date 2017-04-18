526fa4a1c015256487b2056b6342e281edcad21e
Kenzo Yuri <kenzonishitani@gmail.com>
2017-04-18 17:03:12 -0300

    Aplicativo ATM Consultoria

---
+++ AtmEmpresaConsultoria/app/src/main/java/br/com/kenzo/atmempresaconsultoria/ClienteActivity.java
@@ -0,0 +1,14 @@
+package br.com.kenzo.atmempresaconsultoria;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClienteActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente);
+    }
+}
--- AtmEmpresaConsultoria/app/src/main/java/br/com/kenzo/atmempresaconsultoria/MainActivity.java
+++ AtmEmpresaConsultoria/app/src/main/java/br/com/kenzo/atmempresaconsultoria/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
-    private ImageView botaoEmpresa;
+    private ImageView botaoEmpresa, botaoServico, botaoCliente, botaoContato;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,35 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         botaoEmpresa = (ImageView) findViewById(R.id.imvEmpresa);
+        botaoServico = (ImageView) findViewById(R.id.imvServico);
+        botaoCliente = (ImageView) findViewById(R.id.imvCliente);
+        botaoContato = (ImageView) findViewById(R.id.imvContato);
 
         botaoEmpresa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,EmpresaActivity.class));
+            }
+        });
+
+        botaoServico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ServicoActivity.class));
+            }
+        });
+
+        botaoCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ClienteActivity.class));
+            }
+        });
+
+        botaoContato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ContatoActivity.class));
             }
         });
     }
